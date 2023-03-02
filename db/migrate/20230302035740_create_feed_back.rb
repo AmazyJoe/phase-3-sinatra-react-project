2413552000866f893ac1cacd03e837443a1bceb2
AmazyJoe <ochiengolu@gmail.com>
2023-03-02 07:15:16 +0300

    migration done

--- db/migrate/20230302035740_create_feed_back.rb
+++ db/migrate/20230302035740_create_feed_back.rb
@@ -1,4 +1,11 @@
 class CreateFeedBack < ActiveRecord::Migration[6.1]
   def change
+    create_table :feedbacks do |t|
+      t.text :comment
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
   end
 end
+
+ 
--- db/migrate/20230302035801_create_raw_materials.rb
+++ db/migrate/20230302035801_create_raw_materials.rb
@@ -1,4 +1,16 @@
 class CreateRawMaterials < ActiveRecord::Migration[6.1]
   def change
+    create_table :raw_materials do |t|
+      t.string :name
+      t.string :image_url
+      t.references :user, foreign_key: true
+      t.integer :temperature
+      t.string :description
+      t.string :maturity_period
+      t.string :environmental_conditions
+      t.timestamps
+    end
   end
 end
+
+ 
