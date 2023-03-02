41a0ed1af08ed33ab005102a22e2327c0decc333
AmazyJoe <ochiengolu@gmail.com>
2023-03-02 09:19:34 +0300

    server started and routing done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/rawmaterials" do
+    RawMaterial.all.to_json
   end
 
+  get "/feedbacks" do
+    Feedback.all.to_json
+  end
+
+  get "/users" do
+    User.all.to_json
+  end
+  # get "/rawmaterials/id" do
+  #   RawMaterial.all.to_json
+  # end
+  
 end
